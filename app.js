64a9ac3cbc3b690be949c32a7465f98e00f050b2
Srinivas Dharpally <110097436+cnu1328@users.noreply.github.com>
2024-02-25 11:04:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-const dev_db_url = "mongodb+srv://srinudarpally:mongoDBCnu1328@cluster0.7piqncz.mongodb.net/local_library?retryWrites=true&w=majority";
+const dev_db_url = "";
 
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
